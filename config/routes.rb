17b7d24b08d97738dc65decd10a4aa3bda611241
Onyekachi Okereke <robocopkaka@gmail.com>
2018-10-15 18:53:06 +0100

    feat(homePage): add home page
    implement new home page design
    
    [Starts #161216282]

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'home/index'
   devise_for :doctors
   devise_for :patients
   get 'patients/new'
@@ -16,6 +17,6 @@ Rails.application.routes.draw do
   resources :doctors do
     get 'appointments', on: :member
   end
-  root 'doctors#index'
+  root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
